709e2c0de904d3c99210f9007e713018143ad882
Zeke Sikelianos <zeke@sikelianos.com>
2022-11-09 10:57:36 -0800

    use HTTPS url for instance data zip

--- train.py
+++ train.py
@@ -30,35 +30,9 @@ def train():
   version = model.versions.get(MODEL_VERSION)
 
   output = version.predict(
-      seed=1337,
-      adam_beta1=0.9,
-      adam_beta2=0.999,
-      resolution=512,
-      adam_epsilon=1e-08,
-      class_prompt="a photo of a person wearing a top hat",
-      lr_scheduler='constant',
-      instance_data=open("data.zip", "rb"),
-      # instance_data='https://replicate.delivery/pbxt/HkTENyq0Ph5hzI3HgumA1HTZzycUNp8qWL2n2SnzuaHZpIP4/Archive.zip',
-      learning_rate=1e-06,
-      max_grad_norm=1,
-      n_save_sample=4,
-      save_interval=10000,
-      use_8bit_adam=True,
-      instance_prompt="a photo of sks wearing a top hat",
-      max_train_steps=500,
-      num_class_images=50,
-      num_train_epochs=1,
-      save_infer_steps=50,
-      train_batch_size=1,
-      adam_weight_decay=0.01,
-      prior_loss_weight=1,
-      sample_batch_size=4,
-      train_text_encoder=True,
-      save_guidance_scale=7.5,
-      gradient_checkpointing=True,
-      with_prior_preservation=True,
-      gradient_accumulation_steps=1,
-  )
+    instance_prompt= "a photo of sks wearing a top hat",
+    class_prompt= "a photo of a person wearing a top hat",
+    instance_data="https://zeke.github.io/files/dreambooth-training-data.zip")
 
   print("Done training. Output:\n", json.dumps(output))
 
