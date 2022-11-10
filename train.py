d96116e40b4f3e1ebf5aebde04c28c1a8f2adc00
Dominic Baggott <dominic.baggott@gmail.com>
2022-11-10 20:50:31 +0000

    Format with Black

--- train.py
+++ train.py
@@ -10,41 +10,44 @@ import replicate
 MODEL_NAME = "replicate/cog-dreambooth-trainer"
 MODEL_VERSION = "b9a7267b10bb9e5fb19eca853ee9582e8838c8cfe1bf174f4cd50991c857992e"
 
+
 def train():
-  print("Training model...")
+    print("Training model...")
 
-  print("Checking training data")
-  image_count = 0
-  for file in os.listdir("data"):
-    if mimetypes.guess_type(file)[0].startswith("image/"):
-      image_count += 1  
-  if image_count == 0:
-    raise Exception("No images found in data directory")
-
-  print("Zipping training data")
-  with zipfile.ZipFile("data.zip", "w") as zip:
+    print("Checking training data")
+    image_count = 0
     for file in os.listdir("data"):
-      zip.write(os.path.join("data", file))
+        if mimetypes.guess_type(file)[0].startswith("image/"):
+            image_count += 1
+    if image_count == 0:
+        raise Exception("No images found in data directory")
 
-  print(f"Training on Replicate using model {MODEL_NAME}@{MODEL_VERSION}")
-  print(f"https://replicate.com/{MODEL_NAME}/versions/{MODEL_VERSION}")
-  model = replicate.models.get(MODEL_NAME)
-  version = model.versions.get(MODEL_VERSION)
+    print("Zipping training data")
+    with zipfile.ZipFile("data.zip", "w") as zip:
+        for file in os.listdir("data"):
+            zip.write(os.path.join("data", file))
 
-  output = version.predict(
-    instance_prompt= "a photo of sks dog",
-    class_prompt= "a photo of a dog",
-    instance_data=open("data.zip", "rb"))
+    print(f"Training on Replicate using model {MODEL_NAME}@{MODEL_VERSION}")
+    print(f"https://replicate.com/{MODEL_NAME}/versions/{MODEL_VERSION}")
+    model = replicate.models.get(MODEL_NAME)
+    version = model.versions.get(MODEL_VERSION)
 
-  print("Done training. Output:\n", json.dumps(output))
+    output = version.predict(
+        instance_prompt="a photo of sks dog",
+        class_prompt="a photo of a dog",
+        instance_data=open("data.zip", "rb"),
+    )
 
-  # download and unzip trained weights
-  urllib.request.urlretrieve(output, "weights.zip")
+    print("Done training. Output:\n", json.dumps(output))
 
-  with zipfile.ZipFile("weights.zip", 'r') as zip_ref:
-    zip_ref.extractall("weights")
+    # download and unzip trained weights
+    urllib.request.urlretrieve(output, "weights.zip")
 
-  # TODO: download generated weights
+    with zipfile.ZipFile("weights.zip", "r") as zip_ref:
+        zip_ref.extractall("weights")
+
+    # TODO: download generated weights
+
 
 if __name__ == "__main__":
     train()
