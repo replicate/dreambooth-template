471c222fd26fa9fee26a5502f02a6490f9abc5a0
Jesse Andrews <jesse@replicate.com>
2022-12-28 16:50:26 -0500

    redo template with a base of cog-stable-diffusion (#13)
    Sync template predict.py/cog.yaml with cog-stable-diffusion
    
     - update torch/diffusers
     - supports dreambooth based on stable diffusion 1.5, 2.x
     - support NSFW safety_checker
     - helper scripts to download safety_checker in cachable way

--- README.md
+++ README.md
@@ -1,47 +1,17 @@
-# DreamBooth Template
+# Stable Diffusion Dreambooth model template
 
-🦜 Hey! We built an API that automates the whole process outlined below. To get started using that new API, check out the blog post at [replicate.com/blog/dreambooth-api]([https://replicate.com/blog/dreambooth-api])
+This is the template used by [replicate.com dreambooth api](https://replicate.com/blog/dreambooth-api/) to build custom dreambooth models.
 
-If you wish to use the old manual method, you can continue reading below.
+This template is based on [cog-stable-diffusion](https://github.com/replicate/cog-stable-diffusion) which uses diffusers.
 
----
+## Usage
 
-This is a template repo for training and publishing your own custom Stable Diffusion model using Replicate.
+This template is primarily intended for use by [Replicate's DreamBooth API](https://replicate.com/blog/dreambooth-api), which is what you probably want to use to train and publish your own model.
 
-It uses [replicate.com/replicate/dreambooth](https://replicate.com/replicate/dreambooth) to train the model using your custom imagery.
+If you really want to use this template locally, you can do so by following these steps:
 
-## Preqrequisites
-
-- A paid Replicate account
-- Cog and Docker installed locally
-- Access to the Replicate [model publishing beta](https://replicate.com/join)
-- You _don't_ need a local GPU
-
-## Training the model
-
-1. Clone this repo.
-1. Run `pip install -r requirements.txt`
-1. Grab your Replicate API key from https://replicate.com/account and set it as an environment variable: `export REPLICATE_API_TOKEN=<your-token>`.
-1. Run `python train.py`. This takes training images, runs them through the [replicate/dreambooth](https://replicate.com/replicate/dreambooth) model, and downloads the resulting model weights. You can provide images in one of three ways:
-
-   - pass a list of training images with `--images image1.jpg another_image.jpg ...`
-   - pass a zip of images with `--instance-data my-images.zip`
-   - add your images to the data/ folder. (If you use this option, delete the default cute puppy pictures in the folder first! 🐾)
-
-   3-5 images is enough but you can add more. You also need to specify two extra parameters:
-
-   - `--instance-prompt`, the prompt identifying your training images
-   - `--class-prompt`, the prompt describing images in the same class as your training images
-
-   Note that with the default parameters this will take about **12 minutes** to run.
-
-## Publishing the model
-
-The training process creates, downloads, and extracts trained weights for your custom model. If you've got a machine with a GPU, you can run your custom model locally using `cog predict`, but you can also publish it to Replicate so that anyone can run it from the web or using Replicate's API:
-
-1. Create a new model at https://replicate.com/create (You'll need beta access to do this. If you don't have access, you can [join the waitlist](https://replicate.com/join).)
-1. Once the `train` script has finished, publish it to the model you created by running
-
-       python publish.py <your-username/your-model>
-
-   By default this will use the weights downloaded by the `train` script. If you want to use some other weights, you can pass them in with `--weights path/to/your/weights`.
+1. Generate some weights, put them in `weights/` (use our trainer or your own)
+2. Download NSFW safety_checker weights using `script/download-weights`
+3. Install [cog](https://github.com/replicate/cog) & docker
+4. Build `cog build`
+5. Predict `cog predict -i prompt="photo of zzz" -i seed=42` ...
--- predict.py
+++ predict.py
@@ -1,53 +1,94 @@
+import json
 import os
-import shutil
 from typing import List
+
 import torch
-from diffusers import StableDiffusionPipeline, DDIMScheduler
-from pytorch_lightning import seed_everything
 from cog import BasePredictor, Input, Path
+from diffusers import (
+    StableDiffusionPipeline,
+    PNDMScheduler,
+    LMSDiscreteScheduler,
+    DDIMScheduler,
+    EulerDiscreteScheduler,
+    EulerAncestralDiscreteScheduler,
+    DPMSolverMultistepScheduler,
+)
+from diffusers.pipelines.stable_diffusion.safety_checker import (
+    StableDiffusionSafetyChecker,
+)
+from transformers import CLIPFeatureExtractor
 
 
-DREAMBOOTH_MODEL_PATH = "weights"
+SAFETY_MODEL_CACHE = "diffusers-cache"
+SAFETY_MODEL_ID = "CompVis/stable-diffusion-safety-checker"
+
+if not os.path.exists("weights"):
+    raise ValueError("dreambooth weights not found")
+
+DEFAULT_HEIGHT = 512
+DEFAULT_WIDTH = 512
+DEFAULT_SCHEDULER = "DDIM"
+try:
+    with open("weights/args.json") as f:
+        args = json.load(f)
+        if args.get("instance_prompt"):
+            DEFAULT_PROMPT = args["instance_prompt"]
+except:
+    DEFAULT_PROMPT = "a photo of an astronaut riding a horse on mars"
 
 
 class Predictor(BasePredictor):
     def setup(self):
         """Load the model into memory to make running multiple predictions efficient"""
-        scheduler = DDIMScheduler(
-            beta_start=0.00085,
-            beta_end=0.012,
-            beta_schedule="scaled_linear",
-            clip_sample=False,
-            set_alpha_to_one=False,
+        print("Loading Safety pipeline...")
+        safety_checker = StableDiffusionSafetyChecker.from_pretrained(
+            SAFETY_MODEL_ID,
+            cache_dir=SAFETY_MODEL_CACHE,
+            torch_dtype=torch.float16,
+            local_files_only=True,
+        ).to("cuda")
+        feature_extractor = CLIPFeatureExtractor.from_pretrained(
+            "openai/clip-vit-base-patch32", cache_dir=SAFETY_MODEL_CACHE
         )
+
+        print("Loading SD pipeline...")
         self.pipe = StableDiffusionPipeline.from_pretrained(
-            DREAMBOOTH_MODEL_PATH,
-            scheduler=scheduler,
-            safety_checker=None,
+            "weights",
+            safety_checker=safety_checker,
+            feature_extractor=feature_extractor,
             torch_dtype=torch.float16,
         ).to("cuda")
 
     @torch.inference_mode()
-    @torch.cuda.amp.autocast()
     def predict(
         self,
         prompt: str = Input(
             description="Input prompt",
-            default="a cjw cat in a bucket",
+            default=DEFAULT_PROMPT,
         ),
-        negative_prompt: str = Input(description="The prompt NOT to guide the image generation. Ignored when not using guidance", default=None),
+        negative_prompt: str = Input(
+            description="Specify things to not see in the output",
+            default=None,
+        ),
         width: int = Input(
             description="Width of output image. Maximum size is 1024x768 or 768x1024 because of memory limits",
-            choices=[128, 256, 512, 768, 1024],
-            default=512,
+            choices=[128, 256, 384, 448, 512, 576, 640, 704, 768, 832, 896, 960, 1024],
+            default=DEFAULT_WIDTH,
         ),
         height: int = Input(
             description="Height of output image. Maximum size is 1024x768 or 768x1024 because of memory limits",
-            choices=[128, 256, 512, 768, 1024],
-            default=512,
+            choices=[128, 256, 384, 448, 512, 576, 640, 704, 768, 832, 896, 960, 1024],
+            default=DEFAULT_HEIGHT,
+        ),
+        prompt_strength: float = Input(
+            description="Prompt strength when using init image. 1.0 corresponds to full destruction of information in init image",
+            default=0.8,
         ),
         num_outputs: int = Input(
-            description="Number of images to output", choices=[1, 4], default=1
+            description="Number of images to output.",
+            ge=1,
+            le=4,
+            default=1,
         ),
         num_inference_steps: int = Input(
             description="Number of denoising steps", ge=1, le=500, default=50
@@ -55,38 +96,68 @@ class Predictor(BasePredictor):
         guidance_scale: float = Input(
             description="Scale for classifier-free guidance", ge=1, le=20, default=7.5
         ),
+        scheduler: str = Input(
+            default=DEFAULT_SCHEDULER,
+            choices=[
+                "DDIM",
+                "K_EULER",
+                "DPMSolverMultistep",
+                "K_EULER_ANCESTRAL",
+                "PNDM",
+                "KLMS",
+            ],
+            description="Choose a scheduler",
+        ),
         seed: int = Input(
             description="Random seed. Leave blank to randomize the seed", default=None
         ),
     ) -> List[Path]:
         """Run a single prediction on the model"""
-
-        print("Loading pipeline...")
-
         if seed is None:
             seed = int.from_bytes(os.urandom(2), "big")
         print(f"Using seed: {seed}")
 
-        if width == height == 1024:
+        if width * height > 786432:
             raise ValueError(
                 "Maximum size is 1024x768 or 768x1024 pixels, because of memory limits. Please select a lower width or height."
             )
 
-        seed_everything(seed)
+        self.pipe.scheduler = make_scheduler(scheduler, self.pipe.scheduler.config)
 
+        generator = torch.Generator("cuda").manual_seed(seed)
         output = self.pipe(
-            prompt=[prompt] * num_outputs,
-            negative_prompt=[negative_prompt] * num_outputs if negative_prompt is not None else None,
+            prompt=[prompt] * num_outputs if prompt is not None else None,
+            negative_prompt=[negative_prompt] * num_outputs
+            if negative_prompt is not None
+            else None,
             width=width,
             height=height,
-            num_inference_steps=num_inference_steps,
             guidance_scale=guidance_scale,
+            generator=generator,
+            num_inference_steps=num_inference_steps,
         )
 
         output_paths = []
-        for i, sample in enumerate(output["images"]):
-            output_path = f"/tmp/out-{i}.png"
-            sample.save(output_path)
-            output_paths.append(Path(output_path))
+        for i, sample in enumerate(output.images):
+            if output.nsfw_content_detected and not output.nsfw_content_detected[i]:
+                output_path = f"/tmp/out-{i}.png"
+                sample.save(output_path)
+                output_paths.append(Path(output_path))
+
+        if len(output_paths) == 0:
+            raise Exception(
+                f"NSFW content detected. Try running it again, or try a different prompt."
+            )
 
         return output_paths
+
+
+def make_scheduler(name, config):
+    return {
+        "PNDM": PNDMScheduler.from_config(config),
+        "KLMS": LMSDiscreteScheduler.from_config(config),
+        "DDIM": DDIMScheduler.from_config(config),
+        "K_EULER": EulerDiscreteScheduler.from_config(config),
+        "K_EULER_ANCESTRAL": EulerAncestralDiscreteScheduler.from_config(config),
+        "DPMSolverMultistep": DPMSolverMultistepScheduler.from_config(config),
+    }[name]
