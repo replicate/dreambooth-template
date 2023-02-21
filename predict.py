dd0eb91f72a1b085c69131931e097da657cf3218
Jesse Andrews <jesse@replicate.com>
2023-02-21 01:24:10 +0000

    experimental batch predictor

--- predict.py
+++ predict.py
@@ -1,7 +1,9 @@
-import json
+import time
 import os
 from typing import List
+import json
 
+from tqdm.auto import tqdm
 import torch
 from cog import BasePredictor, Input, Path
 from diffusers import (
@@ -19,13 +21,11 @@ from diffusers.pipelines.stable_diffusion.safety_checker import (
 )
 from PIL import Image
 from transformers import CLIPFeatureExtractor
-
+import shutil
+import subprocess
 
 SAFETY_MODEL_CACHE = "diffusers-cache"
 SAFETY_MODEL_ID = "CompVis/stable-diffusion-safety-checker"
-
-if not os.path.exists("weights"):
-    raise ValueError("dreambooth weights not found")
 
 DEFAULT_HEIGHT = 512
 DEFAULT_WIDTH = 512
@@ -34,36 +34,55 @@ DEFAULT_SCHEDULER = "DDIM"
 # grab instance_prompt from weights,
 # unless empty string or not existent
 
-DEFAULT_PROMPT = None
-try:
-    with open("weights/args.json") as f:
-        args = json.load(f)
-        DEFAULT_PROMPT = args["instance_prompt"]
-except:
-    pass
-if not DEFAULT_PROMPT:
-    DEFAULT_PROMPT = "a photo of an astronaut riding a horse on mars"
+DEFAULT_PROMPT = "a photo of an astronaut riding a horse on mars"
 
 
 class Predictor(BasePredictor):
     def setup(self):
-        """Load the model into memory to make running multiple predictions efficient"""
-        print("Loading Safety pipeline...")
         self.safety_checker = StableDiffusionSafetyChecker.from_pretrained(
             SAFETY_MODEL_ID,
             cache_dir=SAFETY_MODEL_CACHE,
             torch_dtype=torch.float16,
             local_files_only=True,
         ).to("cuda")
-        feature_extractor = CLIPFeatureExtractor.from_pretrained(
+        self.feature_extractor = CLIPFeatureExtractor.from_pretrained(
             "openai/clip-vit-base-patch32", cache_dir=SAFETY_MODEL_CACHE
         )
+        self.url = None
 
+    def download_zip_weights_python(self, url):
+        """Download the model weights from the given URL"""
+        print("Downloading weights...")
+
+        if os.path.exists("weights"):
+            shutil.rmtree("weights")
+        os.makedirs("weights")
+
+        import zipfile
+        from io import BytesIO
+        import urllib.request
+
+        url = urllib.request.urlopen(url)
+        with zipfile.ZipFile(BytesIO(url.read())) as zf:
+            zf.extractall("weights")
+
+    def load_weights(self, url):
+        """Load the model into memory to make running multiple predictions efficient"""
+        print("Loading Safety pipeline...")
+
+        if url == self.url:
+            return
+
+        start_time = time.time()
+        self.download_zip_weights_python(url)
+        print("Downloaded weights in {:.2f} seconds".format(time.time() - start_time))
+
+        start_time = time.time()
         print("Loading SD pipeline...")
         self.txt2img_pipe = StableDiffusionPipeline.from_pretrained(
             "weights",
             safety_checker=self.safety_checker,
-            feature_extractor=feature_extractor,
+            feature_extractor=self.feature_extractor,
             torch_dtype=torch.float16,
         ).to("cuda")
 
@@ -76,127 +95,95 @@ class Predictor(BasePredictor):
             safety_checker=self.txt2img_pipe.safety_checker,
             feature_extractor=self.txt2img_pipe.feature_extractor,
         ).to("cuda")
+        print("Loaded pipelines in {:.2f} seconds".format(time.time() - start_time))
+        self.url = url
+
+    def generate_images(self, images, output_dir):
+        with torch.autocast("cuda"), torch.inference_mode():
+            pipeline = self.txt2img_pipe
+            pipeline.set_progress_bar_config(disable=True)
+            for info in tqdm(images, desc="Generating samples"):
+                inputs = info.get("input") or info.get("inputs")
+                name = info["name"]
+                prompt = inputs["prompt"]
+                negative_prompt = inputs.get("negative_prompt")
+                width = int(inputs.get("width", 512))
+                height = int(inputs.get("height", 512))
+                num_outputs = int(inputs.get("num_outputs", 1))
+                disable_safety_check = bool(inputs.get("disable_safety_check", False))
+                num_inference_steps = int(inputs.get("num_inference_steps", 50))
+                guidance_scale = float(inputs.get("guidance_scale", 7.5))
+                scheduler = inputs.get("scheduler", "DDIM")
+                seed = inputs.get("seed")
+                if seed is None:
+                    seed = int.from_bytes(os.urandom(2), "big")
+                else:
+                    seed = int(seed)
+
+                pipeline.scheduler = make_scheduler(
+                    scheduler, pipeline.scheduler.config
+                )
+                if disable_safety_check:
+                    pipeline.safety_checker = None
+                else:
+                    pipeline.safety_checker = self.safety_checker
+
+                generator = torch.Generator("cuda").manual_seed(seed)
+                output = pipeline(
+                    prompt=[prompt] * num_outputs if prompt is not None else None,
+                    negative_prompt=[negative_prompt] * num_outputs
+                    if negative_prompt is not None
+                    else None,
+                    guidance_scale=guidance_scale,
+                    generator=generator,
+                    num_inference_steps=num_inference_steps,
+                    width=width,
+                    height=height,
+                )
+
+                for i, image in enumerate(output.images):
+                    if output.nsfw_content_detected and output.nsfw_content_detected[i]:
+                        print("skipping nsfw detected for", inputs)
+                        continue
+                    image.save(os.path.join(output_dir, f"{name}-{i}.png"))
 
     @torch.inference_mode()
     def predict(
         self,
-        prompt: str = Input(
-            description="Input prompt",
-            default=DEFAULT_PROMPT,
+        images: str = Input(
+            description="JSON input",
         ),
-        negative_prompt: str = Input(
-            description="Specify things to not see in the output",
-            default=None,
-        ),
-        image: Path = Input(
-            description="A starting image from which to generate variations (aka 'img2img'). If this input is set, the `width` and `height` inputs are ignored and the output will have the same dimensions as the input image.",
-            default=None,
-        ),
-        width: int = Input(
-            description="Width of output image. Maximum size is 1024x768 or 768x1024 because of memory limits",
-            choices=[128, 256, 384, 448, 512, 576, 640, 704, 768, 832, 896, 960, 1024],
-            default=DEFAULT_WIDTH,
-        ),
-        height: int = Input(
-            description="Height of output image. Maximum size is 1024x768 or 768x1024 because of memory limits",
-            choices=[128, 256, 384, 448, 512, 576, 640, 704, 768, 832, 896, 960, 1024],
-            default=DEFAULT_HEIGHT,
-        ),
-        prompt_strength: float = Input(
-            description="Prompt strength when using init image. 1.0 corresponds to full destruction of information in init image",
-            default=0.8,
-        ),
-        num_outputs: int = Input(
-            description="Number of images to output.",
-            ge=1,
-            le=4,
-            default=1,
-        ),
-        num_inference_steps: int = Input(
-            description="Number of denoising steps", ge=1, le=500, default=50
-        ),
-        guidance_scale: float = Input(
-            description="Scale for classifier-free guidance", ge=1, le=20, default=7.5
-        ),
-        scheduler: str = Input(
-            default=DEFAULT_SCHEDULER,
-            choices=[
-                "DDIM",
-                "K_EULER",
-                "DPMSolverMultistep",
-                "K_EULER_ANCESTRAL",
-                "PNDM",
-                "KLMS",
-            ],
-            description="Choose a scheduler",
-        ),
-        seed: int = Input(
-            description="Random seed. Leave blank to randomize the seed", default=None
-        ),
-        disable_safety_check: bool = Input(
-            description="Disable safety check. Use at your own risk!", default=False
+        weights: str = Input(
+            description="URL to weights",
         ),
     ) -> List[Path]:
         """Run a single prediction on the model"""
-        if seed is None:
-            seed = int.from_bytes(os.urandom(2), "big")
-        print(f"Using seed: {seed}")
 
-        if width * height > 786432:
-            raise ValueError(
-                "Maximum size is 1024x768 or 768x1024 pixels, because of memory limits. Please select a lower width or height."
-            )
-
-        if image is not None:
-            print("using img2img")
-            pipe = self.img2img_pipe
-            extra_kwargs = {
-                "image": Image.open(image).convert("RGB"),
-                "strength": prompt_strength,
-            }
-        else:
-            print("using txt2img")
-            pipe = self.txt2img_pipe
-            extra_kwargs = {
-                "width": width,
-                "height": height,
-            }
-
-        pipe.scheduler = make_scheduler(scheduler, pipe.scheduler.config)
-
-        generator = torch.Generator("cuda").manual_seed(seed)
-
-        if disable_safety_check:
-            pipe.safety_checker = None
-        else:
-            pipe.safety_checker = self.safety_checker
-
-        output = pipe(
-            prompt=[prompt] * num_outputs if prompt is not None else None,
-            negative_prompt=[negative_prompt] * num_outputs
-            if negative_prompt is not None
-            else None,
-            guidance_scale=guidance_scale,
-            generator=generator,
-            num_inference_steps=num_inference_steps,
-            **extra_kwargs,
+        weights = weights.replace(
+            "https://replicate.delivery/pbxt/",
+            "https://storage.googleapis.com/replicate-files/",
         )
 
-        output_paths = []
-        for i, sample in enumerate(output.images):
-            if output.nsfw_content_detected and output.nsfw_content_detected[i]:
-                continue
+        images_json = json.loads(images)
 
-            output_path = f"/tmp/out-{i}.png"
-            sample.save(output_path)
-            output_paths.append(Path(output_path))
+        if weights is None:
+            raise ValueError("No weights provided")
+        self.load_weights(weights)
 
-        if len(output_paths) == 0:
-            raise Exception(
-                f"NSFW content detected. Try running it again, or try a different prompt."
-            )
+        cog_generated_images = "cog_generated_images"
+        if os.path.exists(cog_generated_images):
+            shutil.rmtree(cog_generated_images)
+        os.makedirs(cog_generated_images)
 
-        return output_paths
+        self.generate_images(images_json, cog_generated_images)
+
+        directory = Path(cog_generated_images)
+
+        results = []
+        for file_path in directory.rglob("*"):
+            print(file_path)
+            results.append(file_path)
+        return results
 
 
 def make_scheduler(name, config):
